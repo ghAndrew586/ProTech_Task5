4f514d1ddf048bc33b3f2c7a2a635c1429727cfa
Andrey Kochetov <and-usmail-586@yandex.ru>
2024-02-06 20:37:18 +0500

    First commit

---
+++ ConsoleProgram/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace ConsoleProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите строку: ");
+            
+            string inputLine = Console.ReadLine();
+
+            while (!Regex.IsMatch(inputLine, "^[a-z]+$"))
+            {
+                if (Regex.Replace(inputLine, " ", "") != "")
+                {
+                    Console.Write("В строке должны быть только латинские буквы в нижнем регистре! Неподходящие символы: ");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(Regex.Replace(Regex.Replace(inputLine, "[a-z]", ""), " ", " 'Пробел' "));
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    Console.WriteLine("Строка не должна быть пустой!");
+                }
+                
+                inputLine = Console.ReadLine();
+            }
+
+            char[] mainLine = inputLine.ToCharArray();
+
+            Console.Write("Выход: ");
+            Array.Reverse(mainLine);
+            string resultLine;
+            Console.ForegroundColor = ConsoleColor.Green;
+            if (mainLine.Length % 2 != 0)
+            {
+                resultLine = new string(mainLine);
+                Array.Reverse(mainLine);
+                resultLine += new string(mainLine);
+
+                Console.WriteLine(resultLine);
+            }
+            else
+            {
+                var lastSegment = new ArraySegment<char>(mainLine, 0, mainLine.Length / 2);
+                var firstSegment = new ArraySegment<char>(mainLine, mainLine.Length / 2, mainLine.Length / 2);
+                resultLine = String.Join("", firstSegment) + (String.Join("", lastSegment));
+                Console.WriteLine(resultLine);
+                Array.Reverse(mainLine);
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+
+            foreach (char letter in mainLine)
+            {
+                Console.Write($"Количество '{letter}': ");
+                Console.WriteLine(resultLine.Count(lt => lt == letter));
+            }
+
+            string maxLine = "";
+
+            foreach (Match match in Regex.Matches(resultLine, "[aeiouy].*[aeiouy]"))
+            {
+                if (maxLine.Length < match.Value.Length)
+                {
+                    maxLine = match.Value;
+                }
+            }
+            
+            if (maxLine != "")
+            {
+                Console.Write("Самая длинная подстрока, которая начинется и заканчивается на гласную букву: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(maxLine);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.Write("Подстроки, которая начинется и заканчивается на гласную букву, ");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("НЕТ");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(" в данной строке!");
+            }
+            
+
+            Console.Write(" . . . Нажмите любую кнопку, чтобы выйти; Enter, чтобы перезапустить  . . . ");
+            char endKey = Console.ReadKey().KeyChar;
+
+            if (endKey == '\r')
+            {
+                System.Diagnostics.Process.Start(Assembly.GetExecutingAssembly().Location);
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
+        }
+    }
+}
